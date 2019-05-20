1e93a6db7cb04b3500fe70badfa7b1845216b336
liuwx <boss@loverweb.cn>
2019-05-20 16:21:40 +0800

    更新一下

--- src/api/controller/index.ts
+++ src/api/controller/index.ts
@@ -4,7 +4,21 @@ export default class extends Base {
         return this.display();
     }
 
+    /**
+     * @api {get} /api/index/list 列表接口
+     * @apiName list
+     * @apiGroup file
+     *
+     * @apiParam {String} pageSize 页面大小.
+     * @apiParam {String} pageNum  页码.
+     *
+     * @apiSuccess {String} pageSize    页面大小.
+     * @apiSuccess {String} pageTotal   总页数.
+     * @apiSuccess {String} pageNum     页码.
+     * @apiSuccess {String} list        数据列表.
+     */
     listAction() {
+        // tslint:disable-next-line: no-console
         return this.success([1, 2, 3]);
     }
 
