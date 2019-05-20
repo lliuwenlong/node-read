0a1d12426fb7092123bbb0b83aa9f67c738e73ec
root <18801295284@163.com>
2019-05-20 15:37:41 +0800

    111

---
+++ app/api/model/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const thinkjs_1 = require("thinkjs");
+class default_1 extends thinkjs_1.think.Model {
+}
+exports.default = default_1;
+//# sourceMappingURL=index.js.map
